d4087ddedf1845167749f1599930efa475a1c616
gujial <gu18647403665@outlook.com>
2026-02-21 10:28:51 +0800

    CI: 添加 nix flake 配置

--- flake.nix
+++ flake.nix
@@ -6,47 +6,35 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            cmake
-            ninja
-            pkg-config
-            qt6.wrapQtAppsHook
-          ];
-
-          buildInputs = with pkgs; [
-            gcc
-            stdenv.cc.cc.lib
-
-            qt6.qtbase
-            qt6.qtmultimedia
-            qt6.qtwayland
-
-            libopus
-
-            gdb
-            clang-tools
-          ];
-
-          QT_QPA_PLATFORM_PLUGIN_PATH = "${pkgs.qt6.qtbase}/lib/qt-6/plugins";
-          QT_PLUGIN_PATH = "${pkgs.qt6.qtbase}/lib/qt-6/plugins";
-        };
-        package = pkgs.stdenv.mkDerivation {
+        # Define the package
+        packages.default = pkgs.stdenv.mkDerivation {
           pname = "voicePhone";
           version = "1.0.0";
           src = self;
+
+          nativeBuildInputs = with pkgs; [
+            cmake
+            ninja
+            pkg-config
+            qt6.wrapQtAppsHook
+          ];
+
+          buildInputs = with pkgs; [
+            qt6.qtbase
+            qt6.qtmultimedia
+            qt6.qtwayland
+            libopus
+          ];
+        };
+
+        # Define the development shell
+        devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             cmake
             ninja
@@ -54,16 +42,18 @@
             qt6.wrapQtAppsHook
           ];
           buildInputs = with pkgs; [
-            gcc
             qt6.qtbase
             qt6.qtmultimedia
             qt6.qtwayland
             libopus
+            gdb
+            clang-tools
           ];
-          cmakeFlags = [ ];
-          installPhase = ''
-            mkdir -p $out/bin
-            cp -r build/* $out/bin/
+          
+          # These help Qt find plugins during development
+          shellHook = ''
+            export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt6.qtbase}/lib/qt-6/plugins"
+            export QT_PLUGIN_PATH="${pkgs.qt6.qtbase}/lib/qt-6/plugins"
           '';
         };
       }
